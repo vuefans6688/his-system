2298b5f2b28c766b83227c5e83225cbb486fe2ca
小皮测试 <60128997+vuefans6688@users.noreply.github.com>
2020-12-19 21:14:38 +0800

    Update inputLimit.js

--- src/utils/inputLimit.js
+++ src/utils/inputLimit.js
@@ -1,11 +1,11 @@
 // 控制只能输入正整数
 export function intLimit(value, boundary = 0) {      
   value = value.toString()
-  const validArray = [
+  const validates = [
     ['^0(\\d+)$', '$1'],  // 禁止录入整数部分两位以上，但首位必须为0
     ['[^\\d]+', '']  // 禁止录入任何非数字
   ]
-  validArray.forEach(item => {
+  validates.forEach(item => {
     const matches = new RegExp(item[0])
     value = value.replace(matches, item[1])
   })
@@ -18,13 +18,13 @@ export function intLimit(value, boundary = 0) {
 // 控制小数输入并限制位数
 export function floatLimit(value, bit) { 
   value = value.toString()
-  const validArray = [
+  const validates = [
     ['^0(\\d+)$', '$1'],  // 禁止录入整数部分两位以上，但首位必须为0
     ['[^\\d\\.]+', ''],  // 禁止录入任何非数字和点
     ['\\.(\\d?)\\.+', '.$1'],  // 禁止录入两个以上的点
     [`^(\\d+\\.\\d{${bit}}).+`, '$1']  // 禁止录入小数点后n位以上
   ]
-  validArray.forEach(item => {
+  validates.forEach(item => {
     const matches = new RegExp(item[0])
     value = value.replace(matches, item[1])
   })
